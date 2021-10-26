fefaea988c844a5650bc214839e2203ef887c253
Brannon Starnes <brannlee808@gmail.com>
2021-10-25 21:15:44 -0700

    added components to app

---
+++ src/HornedBeast.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class HornedBeast extends Component {
+    render(){
+        return(
+            <><h2>{this.props.title}</h2>
+            <img src={this.props.imgUrl} alt={this.props.title} title={this.props.title} />
+            <p>{this.props.description}</p></>
+        )
+    }
+}
+
+export default HornedBeast;
