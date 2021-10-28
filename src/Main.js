671202a1438b6cdd057b5d8b99f04d542f6ac07b
Brannon Starnes <brannlee808@gmail.com>
2021-10-28 09:56:14 -0700

    refactored modal location

--- src/Main.js
+++ src/Main.js
@@ -2,45 +2,21 @@ import { Component } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import HornedBeast from "./HornedBeast";
-import ImageModal from "./components/ImageModal.js"
 
 
 class Main extends Component {
     
-  constructor(props){
-    super(props);
-    this.state = {
-      show: false,
-      chosenBeast: null
-    }
-}
 
-  chooseBeast = (beastProfile) => {
-    this.setState({
-      show: true,
-      chosenBeast: beastProfile
-    })
-  };
-
-
-
-  hideModal = () => {
-    this.setState({ 
-      show: false
-    })
-  };
-    
     render(){
         return(
           <>
             <Container id = 'mainBox'>
               <Row xs={1} sm={2} md={3} lg={4}>
                 {this.props.beastData.map(beast => (
-                  <HornedBeast beastProfile ={beast} chooseBeast={this.chooseBeast}/>
+                  <HornedBeast beastProfile ={beast} chooseBeast={this.props.chooseBeast}/>
                 ))}  
               </Row>
             </Container>
-            <ImageModal show={this.state.show} chosenBeast={this.state.chosenBeast} hideModal={this.hideModal}></ImageModal>
           </>
         )
     }
